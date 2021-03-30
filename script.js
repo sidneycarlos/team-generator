3c42fcc0cf409f9a23b1f3e508e0c60d0d7383fb
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-03-30 09:29:56 +0000

    features: modif

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //stockage des participants dans une variable
 
-var attendant = {
+var attd = {
     1 : "Alexandre", 2 : "Fanny", 3 : "Tamara", 4 : "Marylise", 5 : "Maxime",
     6 : "Laurène", 7 : "Yohan", 8 : "Juan", 9 : "Maïalen", 10 : "Vincent",
     11 : "Noureddine", 12 : "Lucas", 13 : "Raphael", 14 : "Valentin", 15 : "Sidney"
 }
+var group1 = [];
+var group2 = [];
 
-//générer un nombre entre 0 et 14 qui correspond à l'index de attendant
 /*function generatePosition() {
     var randomiser = Math.floor(Math.random()*15);
     return randomiser;
@@ -15,32 +16,20 @@ var attendant = {
 
 
 //chercher dans la variable les index correspondants aux nombres générés et n'en conserver que 8
-/*var result = [];
-for (var i in attendant) {
-    if (i == randomiser) {
-        result[number] = 
-    }
-    console.log(`${number} : ${attendant[number]}`)
-}*/
-function generatePosition() {
-    var randomiser = Math.floor(Math.random()*15); //génère chiffre aléatoire entre 0 et 15
-    for (var i in attendant) { // j'itère dans ma variable attendant
-        while (i <= 8) { //je veux ne garder que les 8 premières itérations pour cela il faut qu eje stocke dans un tableau les nombres issu du randomiser
-            result[i] = attendant[i];
-            i++
-        }
-        /*if (i < attendant.length) {
-            i++;
-            
-    
-        }*/
-    }
-    console.log(randomiser)
-    return result[i];
 
+ //générer un nombre entre 0 et 14 qui correspond à l'index de attendant
+ function generatePosition() {
+    var randomiser = Math.floor(Math.random()*15);
+    for (let i = 0; i < 8; i++) {
+        group1[i] = randomiser // j'itère dans ma variable attendant
+
+//TODO => pour cela il faut que je stocke dans un tableau les nombres issu du randomiser
+//ensuite le tableau group1 ne devra stocker que les 8 premières itérations 
+
+    }
+    return group1;
 }
-var result = [];
+
 
 
 console.log(generatePosition())
-console.log(result);
