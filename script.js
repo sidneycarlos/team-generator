121f2e54d8ac60a63038ab2f6d4a9f33d9ecb482
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-04-01 12:37:38 +0000

    minor modif

--- script.js
+++ script.js
@@ -6,26 +6,6 @@ var attd = {
     11: "Noureddine", 12: "Lucas", 13: "Raphael", 14: "Valentin", 15: "Sidney"
 }
 
-/** A implémenter dans le code
- * var attd = [
-    {id :1, name: "Alexandre", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :2, name: "Fanny", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :3, name: "Tamara", gender : "F", availability: {monday:false, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :4, name: "Marylise", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :5, name: "Maxime", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :6, name: "Laurène", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :7, name: "Yohan", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :8, name: "Juan", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :9, name: "Maïalen", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :10, name: "Vincent", gender : "M", availability: {monday:true, tuesday:true, wednesday:false,thursday:true, friday:true}, partner: false},
-    {id :11, name: "Noureddine", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :12, name: "Lucas", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: true},
-    {id :13, name: "Raphael", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :14, name: "Valentin", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: false},
-    {id :15, name: "Sidney", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: true}
-]
- */
-
 
 var number = [];
 var groupIndex1 = [];
@@ -37,19 +17,26 @@ var frameDistanciel = document.getElementById("distant");
 var bouton = document.getElementById("generator");
 
 //démarrer la génération de groupe par le clic
-bouton.addEventListener("click", function() {
+bouton.addEventListener("click", function () {
     framePresentiel.innerText = getGroupName1(getNumber());
     frameDistanciel.innerText = getGroupName2(getNumber());
-    framePresentiel.style.backgroundColor="white";
-    framePresentiel.style.padding="10px 15px"
-    framePresentiel.style.opacity="0.9"
-    framePresentiel.style.color="#7F3FBF"
-    frameDistanciel.style.backgroundColor="white";
-    frameDistanciel.style.padding="10px 15px"
-    frameDistanciel.style.opacity="0.9"
-    frameDistanciel.style.color="#7F3FBF"
+
+    function styleOfBlocks() {
+        framePresentiel.style.backgroundColor = "white";
+        framePresentiel.style.padding = "10px 15px"
+        framePresentiel.style.opacity = "0.9"
+        framePresentiel.style.color = "#7F3FBF"
+        frameDistanciel.style.backgroundColor = "white";
+        frameDistanciel.style.padding = "10px 15px"
+        frameDistanciel.style.opacity = "0.9"
+        frameDistanciel.style.color = "#7F3FBF"
+    }
+    styleOfBlocks()
 
 })
+
+
+
 //Récupérer des nombres aléatoires
 function getNumber() {
     while (number.length < 15) { // générer une liste de 15 valeurs
@@ -79,6 +66,20 @@ function getGroupName2() {
     return groupNamed2
 }
 
+
+//récupérer le nom de attd qui correspond aux index de 8 à 15
+function getAGroup() {
+    groupIndex2 = number.slice(8, 15) //stocker les 7 derniers index
+    for (let i = 0; i < groupIndex2.length; i++) {
+        for (let j in attd) {
+            attd.name.i = attd[groupIndex2[i]]
+
+        }
+        var attdList2 = attd[groupIndex2[i]]; //passer dans le tableau et donner la valeur de chaque index (prénom)
+        groupNamed2[i] = attdList2
+    }
+    return groupNamed2
+}
 /** A implémenter dans le code
  * var attd = [
     {id :1, name: "Alexandre", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
