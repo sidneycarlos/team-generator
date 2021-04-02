969ab46cf7ab73112c403efc8f7659654da0d9bd
sidney_carlos <bwb.carlos.s@fondespierre.com>
2021-04-02 05:50:19 +0000

    final refacto: it works!

--- script.js
+++ script.js
@@ -1,121 +1,97 @@
 //stockage des participants dans une variable
 
-var attd = [
-    "Alexandre","Fanny", "Tamara",
-    "Marylise","Maxime","Laurène",
-    "Yohan","Juan","Maïalen",
-    "Vincent","Noureddine","Lucas",
-    "Raphael","Valentin","Sidney"
-    ]
-    
-    
-    var names = [];
-    var group1 = [];
-    var group2 = [];
-    var framePresentiel = document.getElementById("here");
-    var frameDistanciel = document.getElementById("distant");
-    var bouton = document.getElementById("generator");
-    
-    
-    //démarrer la génération de groupe par le clic
-    bouton.addEventListener("click", function() {
-        framePresentiel.innerText = divideInGroups(getNames(getNumbers), 0, 8);;
-        frameDistanciel.innerText = divideInGroups(getNames(getNumbers), 8, 15);
-        framePresentiel.style.backgroundColor="white";
-        framePresentiel.style.padding="10px 15px"
-        framePresentiel.style.opacity="0.9"
-        framePresentiel.style.color="#7F3FBF"
-        frameDistanciel.style.backgroundColor="white";
-        frameDistanciel.style.padding="10px 15px"
-        frameDistanciel.style.opacity="0.9"
-        frameDistanciel.style.color="#7F3FBF"
-    
-    })
-    //Récupérer des nombres aléatoires
-    function getNumbers() {
-        var number = [];
-        while (number.length < 15) { // générer une liste de 15 valeurs
-            var randomiser = Math.floor(Math.random() * 15); //générer 15 nombres aléatoires qui serviront d'index
-            if (number.indexOf(randomiser) === -1) number.push(randomiser) //gérer les doublons
-        }
-        return number;
+var trainees = [
+    "Alexandre", "Fanny", "Tamara",
+    "Marylise", "Maxime", "Laurène",
+    "Yohan", "Juan", "Maïalen",
+    "Vincent", "Noureddine", "Lucas",
+    "Raphael", "Valentin", "Sidney"
+]
+
+var number = [];
+var names = [];
+var presentiel = document.getElementById("here");
+var distanciel = document.getElementById("distant");
+var bouton = document.getElementById("generator");
+var reset = document.getElementById("reset")
+
+
+
+/**
+ * Récupère 15 nombres
+ * @returns les nombres dans un ordre aléatoire
+ */
+function getNumbers() {
+    while (number.length < 15) { // générer une liste de 15 valeurs
+        var randomiser = Math.floor(Math.random() * 15); //générer 15 nombres aléatoires qui serviront d'index
+        if (number.indexOf(randomiser) === -1) number.push(randomiser) //gérer les doublons
     }
-    
-    function getNames(groupIndex) {
-        for (let i = 0; i < groupIndex.length; i++) {
-            names[i] = attd[groupIndex[i]]; //passer dans le tableau et donner la valeur de chaque index(prénom)
-        }
-        return names;
+    return number;
+}
+
+/**
+ * Récupère les noms dans le nouvel ordre
+ * @param {*} groupIndex correspond à la liste des index aléatoires
+ * @returns les noms des stagiaires dans l'ordre aléatoire
+ */
+function getNames(groupIndex) {
+    for (let i = 0; i < groupIndex.length; i++) {
+        names[i] = trainees[groupIndex[i]]; //passer dans le tableau et donner la valeur de chaque index(prénom)
     }
-    getNames(getNumbers())
-    
-    function divideInGroups(arr, minIndex, maxIndex) {
-        var groups = arr.slice(minIndex, maxIndex)
-        return groups
+    return names;
+}
+
+/**
+ * Récupère la liste selon les index
+ * @param {*} arr liste des noms aléatoires
+ * @param {*} minIndex index de début de la liste
+ * @param {*} maxIndex index de fin de la liste
+ * @returns un groupe selon l'index de début et l'index de fin
+ */
+function divideInGroups(arr, minIndex, maxIndex) {
+    var groups = arr.slice(minIndex, maxIndex)
+    return groups
+}
+
+
+//démarrer la génération de groupe par le clic
+bouton.addEventListener("click", () => {
+    presentiel.innerText = divideInGroups(getNames(getNumbers()), 0, 8);;
+    distanciel.innerText = divideInGroups(getNames(getNumbers()), 8, 15);
+    presentiel.style.backgroundColor = "white";
+    presentiel.style.padding = "10px 15px"
+    presentiel.style.opacity = "0.9"
+    presentiel.style.color = "#black"
+    distanciel.style.backgroundColor = "white";
+    distanciel.style.padding = "10px 15px"
+    distanciel.style.opacity = "0.9"
+    distanciel.style.color = "#black"
+})
+
+//réinitialisation des résultats par le clic
+reset.addEventListener("click", () => {
+    if (number.length == 15) {
+        number = []
     }
-    
-    group1= divideInGroups(getNames(getNumbers), 0, 8);
-    group2= divideInGroups(getNames(getNumbers), 8, 15);
-    
-    console.log(group1);
-    console.log(group2);
-    
-    /*test
-    function getGroupName1(arr, minIndex, maxIndex) {
-        groupIndex = arr.slice(minIndex, maxIndex) //stocker les 8 premiers index
-        for (let i = 0; i < groupIndex.length; i++) {
-            groupNamed1[i] = attd[groupIndex1[i]]; //passer dans le tableau et donner la valeur de chaque index(prénom)
-        }
-        return groupNamed1
-    }*/
-    
-    //Récupérer une partie de la liste
-    /*function getGroupName1(groupIndex1) {
-        groupIndex1 = number.slice(0, 8) //stocker les 8 premiers index
-        for (let i = 0; i < groupIndex1.length; i++) {
-            groupNamed1[i] = attd[groupIndex1[i]]; //passer dans le tableau et donner la valeur de chaque index(prénom)
-        }
-        return groupNamed1
-    }
-    
-    //Récupérer l'autre partie de la liste
-    function getGroupName2(groupIndex2) {
-        groupIndex2 = number.slice(8, 15) //stocker les 7 derniers index
-        for (let i = 0; i < groupIndex2.length; i++) {
-            groupNamed2[i] = attd[groupIndex2[i]]; //passer dans le tableau et donner la valeur de chaque index (prénom)
-        }
-        return groupNamed2
-    }
-    
-    //Fonction pour vider la table
-    myResetTable.addEventListener("click", (e)=> {
-        e.preventDefault();
-    
-    document.getElementById("allGroup").innerHTML = "";
-    document.getElementById("groupe1").innerHTML = "";
-    document.getElementById("groupe2").innerHTML = "";
-    
-    
-    
-    });*/
-    
-    
-    /** A implémenter dans le code
-     * var attd = [
-        {id :1, name: "Alexandre", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :2, name: "Fanny", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :3, name: "Tamara", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :4, name: "Marylise", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :5, name: "Maxime", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :6, name: "Laurène", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :7, name: "Yohan", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :8, name: "Juan", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :9, name: "Maïalen", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :10, name: "Vincent", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :11, name: "Noureddine", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :12, name: "Lucas", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :13, name: "Raphael", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :14, name: "Valentin", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
-        {id :15, name: "Sidney", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"}
-    ]
-     */
+})
+
+
+/** A implémenter dans le code
+ * var trainees = [
+    {id :1, name: "Alexandre", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :2, name: "Fanny", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :3, name: "Tamara", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :4, name: "Marylise", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :5, name: "Maxime", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :6, name: "Laurène", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :7, name: "Yohan", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :8, name: "Juan", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :9, name: "Maïalen", gender : "F", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :10, name: "Vincent", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :11, name: "Noureddine", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :12, name: "Lucas", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :13, name: "Raphael", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :14, name: "Valentin", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"},
+    {id :15, name: "Sidney", gender : "M", availability: {monday:true, tuesday:true, wednesday:true,thursday:true, friday:true}, partner: "N"}
+]
+ */
